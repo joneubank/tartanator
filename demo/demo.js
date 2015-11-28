a4579ced530d953a6be657a4fe9af47e91346098
Jon Eubank <joneubank@gmail.com>
2015-11-28 00:21:25 -0500

    hud ui refactor, added jquery ui

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,64 @@
 "use strict";
 
+function randomizeTartan()
+{
+    // var color1 = Tartanator.Color.make(137, 71, 31);
+    // var color2 = Tartanator.Color.make(21, 39, 28);
+    var color1 = Tartanator.Color.random();
+    var color2 = Tartanator.Color.random();
+
+    var plaid = Tartanator.blank();
+    plaid.addHorizontal(10, color1);
+    plaid.addHorizontal(2, color2);
+    plaid.addHorizontal(1, color1);
+    plaid.addHorizontal(8, color2);
+    plaid.addHorizontal(1, color1);
+    plaid.addHorizontal(2, color2);
+    plaid.addVertical(10, color1);
+    plaid.addVertical(2, color2);
+    plaid.addVertical(1, color1);
+    plaid.addVertical(8, color2);
+    plaid.addVertical(1, color1);
+    plaid.addVertical(2, color2);
+
+    setBackground(plaid);
+}
+
+
+//Catchphrase:
+var phrases = [
+    "\"I'll be Plaid...\" -The Tartanator",
+    "This. Is. Tartaaaan!",
+    "Clash of the Tartans"
+];
+
+var currentPassphrase = -1;
+function setCatchphrase()
+{
+    var phrasetag = document.getElementById("tartan-random");
+    var selectedPhrase = currentPassphrase;
+    while(selectedPhrase == currentPassphrase) {
+        selectedPhrase = Math.random()*phrases.length|0;
+    } 
+    currentPassphrase = selectedPhrase;
+    phrasetag.innerHTML = phrases[selectedPhrase];
+};
+
+/*
+* HUD setup:
+*/
+$("button").button();
+$("#tartan-random").click(function( event ) {
+    randomizeTartan();
+    setCatchphrase();
+});
+
+//Background updates:
 function setBackground(plaid)
 {
+    var width = 200;
+    var height = 200;
+
     plaid.build({width:width, height:height});
     var dataUrl = plaid.getDataUrl();
 
@@ -11,29 +68,8 @@ function setBackground(plaid)
 }
 
 
-
-// var color1 = Tartanator.Color.make(200, 40, 10);
-// var color2 = Tartanator.Color.make(10, 10, 90);
-var color1 = Tartanator.Color.random();
-var color2 = Tartanator.Color.random();
-
-var plaid = Tartanator.blank();
-plaid.addHorizontal(10, color1);
-plaid.addHorizontal(2, color2);
-plaid.addHorizontal(1, color1);
-plaid.addHorizontal(8, color2);
-plaid.addHorizontal(1, color1);
-plaid.addHorizontal(2, color2);
-plaid.addVertical(10, color1);
-plaid.addVertical(2, color2);
-plaid.addVertical(1, color1);
-plaid.addVertical(8, color2);
-plaid.addVertical(1, color1);
-plaid.addVertical(2, color2);
-
-var width = 200;
-var height = 200;
-
-
-setBackground(plaid);
-
+//On Load
+$(function(){
+    randomizeTartan();
+    setCatchphrase();
+});
